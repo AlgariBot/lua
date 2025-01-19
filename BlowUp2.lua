5f2880325f6876d9e85edbc8ef8c9357fed08f4f
ㅤ <algaryalga@gmail.com>
2025-01-19 19:58:59 +0700

    Update BlowUp2.lua

--- BlowUp2.lua
+++ BlowUp2.lua
@@ -251,5 +251,5 @@ local function se(nam, Ida, Idb, Idc, Idd,Idf,Idg,duration, sp)
     end)
 end
 
-se("Animation", "87684948", "182749109", "10714360343", "106772613", "182789003", "0", 1.8, 0)
+se("Animation", "87684948", "182749109", "10714360343", "106772613", "182789003", "0", 1.7, 0)
 dashb("Blowjob v2")
